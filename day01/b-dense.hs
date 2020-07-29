db883334b346ce311f8aa8ce16940e9175044a90
Wong Meng Weng <mwwong@smu.edu.sg>
2020-07-30 01:05:34 +0800

    reword the intermediately-dense version to be more approachable

--- day01/b-dense.hs
+++ day01/b-dense.hs
@@ -1,10 +1,14 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-16.2 script
 -- here we use parentheses to give familiar comfort, resembling traditional function calls
--- we introduce a lambda, and <$> notation for map / fmap
+-- we introduce a lambda, and <$> notation as an alternative to map / fmap
 
 main = do
   input <- getContents
-  print(sum( (\givenMass -> givenMass `div` 3 - 2) <$> read <$> lines input))
+  print(sum( map                                   -- takes two arguments! syntax is lisp-like.
+             (\givenMass -> givenMass `div` 3 - 2) -- first argument:  run a lambda function over
+             (read <$> lines input)                -- second argument: a list of splitted strings converted to integers
+           )
+       )
 
 
