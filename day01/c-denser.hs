23e81f26406411a1594c5756e39d29bee0ec3db6
Meng Weng Wong <mengwong@gmail.com>
2022-12-01 22:09:01 +0800

    a fresh start for 2022

--- day01/c-denser.hs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env stack
--- stack --resolver lts-16.2 script
--- while (+2) would work, it is necessary to say (subtract 2), because (-2) is just a number
-
-main = do
-  input <- getContents
-  print $ sum $ ((subtract 2) . (`div` 3)) <$> read <$> lines input
