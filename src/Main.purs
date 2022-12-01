0af6a2ab126f8a6897fb86a5808430aec9d6b0c4
Meng Weng Wong <mengwong@gmail.com>
2022-12-02 00:26:52 +0800

    more idiomatically

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,20 @@
 module Main where
 
 import Prelude
-import Data.Tuple (Tuple(..), fst, snd)
+import Data.Tuple (Tuple)
+import Data.Tuple.Nested ((/\))
 import Data.Array (filter)
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  let split = splitPosAndNeg [1, -2, 3, -4, 5, -6, -7, 8]
-      pos = fst split
-      neg = snd split
+  let pos /\ neg = splitPosAndNeg [1, -2, 3, -4, 5, -6, -7, 8]
   log ("pos = " <> show pos)
   log ("neg = " <> show neg)
 
 splitPosAndNeg :: Array Int -> Tuple (Array Int) (Array Int)
-splitPosAndNeg xs = Tuple (filter (\x -> x>=0) xs)
-                          (filter (\x -> x< 0) xs)
+splitPosAndNeg xs = (filter (\x -> x>=0) xs) 
+                    /\
+                    (filter (\x -> x< 0) xs)
+
