f48c2860099f6cabaf23e451bb05c44d861fbda8
Meng Weng Wong <mengwong@gmail.com>
2022-12-04 15:19:24 +0800

    move utility functions down

--- apps/aoc2022-day04.hs
+++ apps/aoc2022-day04.hs
@@ -3,18 +3,23 @@
 
 import Data.List.Split (splitOn)
 
-contains :: (Ord a) => [a] -> [a] -> Bool
-a `contains` b = minimum a <= minimum b && maximum b <= maximum a
---                       a------------b============b------------a
-overlaps :: (Ord a) => [a] -> [a] -> Bool
-a `overlaps` b = minimum a <= minimum b && minimum b <= maximum a
---                       a------------b=========================a=====b
---                       a------------b-------------------------a
 -- for full theoretical treatment, see Allen's Interval Algebra
 main :: IO ()
 main = do
   input <- lines <$> getContents
-  let elves = [ (elf1,elf2) | line <- input, let [elf1, elf2] = fmap (read :: String -> Int) . splitOn "-" <$> splitOn "," line ]
+  let elves = [ (elf1,elf2) | line <- input
+                            , let [elf1, elf2] = fmap (read :: String -> Int) .
+                                                 splitOn "-" <$>
+                                                 splitOn "," line ]
   print $ length $ filter id [ orViceVersa elf1 contains elf2 | (elf1,elf2) <- elves ]
   print $ length $ filter id [ orViceVersa elf1 overlaps elf2 | (elf1,elf2) <- elves ]
   where orViceVersa x f y = f x y || f y x
+
+contains,overlaps :: (Ord a) => [a] -> [a] -> Bool
+
+a `contains` b = minimum a <= minimum b && maximum b <= maximum a
+--                       a------------b============b------------a
+
+a `overlaps` b = minimum a <= minimum b && minimum b <= maximum a
+--                       a------------b=========================a=====b
+--                       a------------b-------------------------a
