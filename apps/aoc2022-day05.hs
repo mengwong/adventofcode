c9ae9015368be721439eaa4a79da334bf41d7a4d
Meng Weng Wong <mengwong@gmail.com>
2022-12-05 20:36:53 +0800

    refactor, shrink further

--- apps/aoc2022-day05.hs
+++ apps/aoc2022-day05.hs
@@ -5,7 +5,7 @@ import Prelude       hiding (head, tail, drop, take)
 import Data.Vector   hiding (mapMaybe, forM_, reverse, transpose)
 import Text.Megaparsec      (parseMaybe, many, some, Parsec)
 import Text.Megaparsec.Char (char, numberChar, upperChar, space, string)
-import Data.Maybe           (mapMaybe)
+import Data.Maybe           (mapMaybe, fromMaybe)
 import Data.List     as DL  (reverse, transpose)
 import Data.List.Split      (splitOn)
 import Control.Monad        (forM_)
@@ -23,21 +23,16 @@ main = do
                                        <$> (string "move " *> int <* space)
                                        <*> (string "from " *> int <* space)
                                        <*> (string "to "   *> int ))) origMoves
-  forM_ [move1, move2] $ \move -> do
-    let after = Prelude.foldl move stacks moves
+  forM_ [moveBy (Just 1), moveBy Nothing] $ \m -> do
+    let after = Prelude.foldl m stacks moves
     putStrLn $ toList (head <$> tail after)
 
-move1 :: Stacks -> (Int, Int, Int) -> Stacks
-move1 orig (n, from, to)
-  | n > 0 = let new = orig // [(to,   cons (head (orig ! from)) (orig ! to))
-                              ,(from,       tail (orig ! from))]
-            in move1 new (n-1, from, to)
+moveBy :: Maybe Int -> Stacks -> (Int, Int, Int) -> Stacks
+moveBy capacity orig (n, from, to)
+  | n > 0 = let new = orig // [(to,   take cap (orig ! from) <> (orig ! to))
+                              ,(from, drop cap (orig ! from))]
+            in moveBy capacity new (n-cap, from, to)
   | otherwise = orig
-
-move2 :: Stacks -> (Int, Int, Int) -> Stacks
-move2 orig (n, from, to) =
-  orig // [ (to,   take n (orig ! from) <> (orig ! to))
-          , (from, drop n (orig ! from)) ]
+  where cap = fromMaybe n capacity
 
 int :: Parser Int = read <$> some numberChar
-
