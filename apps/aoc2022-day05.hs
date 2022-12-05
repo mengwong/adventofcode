ff391f91b5c619d70136a8a8e32adaf5f0c73b71
Meng Weng Wong <mengwong@gmail.com>
2022-12-05 20:15:43 +0800

    clean up the imports a bit, shorten

--- apps/aoc2022-day05.hs
+++ apps/aoc2022-day05.hs
@@ -1,36 +1,31 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-20.2 script
 
-import Prelude hiding (head, tail, drop, take)
-import Text.Megaparsec ( parseMaybe, many, some, sepBy1, Parsec, eof, (<|>) )
-import Text.Megaparsec.Char ( char, numberChar, upperChar, space, eol, string )
-import Data.Maybe (fromMaybe, mapMaybe)
-import           Data.Vector as Vec hiding (mapMaybe, forM_)
-import qualified Data.List as DL
-import Control.Monad (forM_)
-import Data.List.Split (splitOn)
+import Prelude       hiding (head, tail, drop, take)
+import Data.Vector   hiding (mapMaybe, forM_, reverse, transpose)
+import Text.Megaparsec      (parseMaybe, many, some, Parsec)
+import Text.Megaparsec.Char (char, numberChar, upperChar, space, string)
+import Data.Maybe           (mapMaybe)
+import Data.List     as DL  (reverse, transpose)
+import Data.List.Split      (splitOn)
+import Control.Monad        (forM_)
 
 type Parser = Parsec () String
+type Stacks = Vector (Vector Char)
 
-type Stacks = Vec.Vector (Vec.Vector Char)
-
--- for full theoretical treatment, see Allen's Interval Algebra
 main :: IO ()
 main = do
-  [origStacks, origMoves] <- splitOn [""] <$> lines <$> getContents
-  let stripped = DL.transpose $ DL.reverse $ origStacks
-      stacksL = "" : (DL.reverse <$> mapMaybe (parseMaybe ( int *> some upperChar <* space)) stripped)
-      stacks = fromList (fromList <$> stacksL) -- convert to Vector
-  let moves = mapMaybe (parseMaybe ((,,)
-                                     <$> (string "move " *> int <* space)
-                                     <*> (string "from " *> int <* space)
-                                     <*> (string "to "   *> int ))) origMoves
+  [origStacks, origMoves] <- splitOn [""] . lines <$> getContents
+  let stripped = transpose $ reverse origStacks
+      stacksL  = "" : (mapMaybe (parseMaybe ( int *> some upperChar <* space)) stripped)
+      stacks   = fromList (fromList . reverse <$> stacksL) -- convert to Vector
+      moves    = mapMaybe (parseMaybe ((,,)
+                                       <$> (string "move " *> int <* space)
+                                       <*> (string "from " *> int <* space)
+                                       <*> (string "to "   *> int ))) origMoves
   forM_ [move1, move2] $ \move -> do
     let after = Prelude.foldl move stacks moves
-    putStrLn $ Vec.toList (head <$> tail after)
-
-int :: Parser Int
-int = read <$> some numberChar
+    putStrLn $ toList (head <$> tail after)
 
 move1 :: Stacks -> (Int, Int, Int) -> Stacks
 move1 orig (n, from, to)
@@ -41,7 +36,8 @@ move1 orig (n, from, to)
 
 move2 :: Stacks -> (Int, Int, Int) -> Stacks
 move2 orig (n, from, to) =
-  orig // [(to,   take n (orig ! from) <> (orig ! to))
-          ,(from, drop n (orig ! from))]
+  orig // [ (to,   take n (orig ! from) <> (orig ! to))
+          , (from, drop n (orig ! from)) ]
 
+int :: Parser Int = read <$> some numberChar
 
