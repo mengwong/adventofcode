977008c205dedd4403c75b538fa782b9ba7f0a8a
Meng Weng Wong <mengwong@gmail.com>
2022-12-30 01:28:25 +0800

    day 10: any terser and this will be unreadable

--- src/Lib10.hs
+++ src/Lib10.hs
@@ -7,8 +7,8 @@ main = do
   print $ sum [ c * x | (c,x) <- log, c `elem` [20,60..220] ]
   putStrLn $ concat [ pixel (c,x) xpos ++ if xpos == width - 1 then "\n" else ""
                     | (c,x) <- log, let xpos = ((c-1) `mod` width) ]
-  where tick c x (["addx",ns]:xs)  = [(c+1,x),(c+2,x + read ns)] ++ tick (c+2) (x + read ns) xs
-        tick c x (["noop"]   :xs)  = [(c+1,x)]                   ++ tick (c+1) x             xs
+  where tick c x (["addx",ns]:xs)  = let x' = x + read ns in [(c+1,x),(c+2,x')] ++ tick (c+2) x' xs
+        tick c x (["noop"]   :xs)  =                         [(c+1,x)         ] ++ tick (c+1) x  xs
         tick c x []                = []
         pixel (cx,rx) c | abs (c - rx) <= 1 = "#"
                         | otherwise         = " "
