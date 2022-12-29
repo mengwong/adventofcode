88a78efd63cf334f732cf783e03a9230bceca283
Meng Weng Wong <mengwong@gmail.com>
2022-12-30 01:19:34 +0800

    even terser

--- src/Lib10.hs
+++ src/Lib10.hs
@@ -3,15 +3,12 @@ module Lib10 where
 main :: IO ()
 main = do
   input <- fmap words <$> (lines <$> getContents)
-  let log = fillGaps $ scanl tick (1,1) input; width = 40
+  let log = tick 1 1 input; width = 40
   print $ sum [ c * x | (c,x) <- log, c `elem` [20,60..220] ]
-  putStrLn $ concat [ brightDark (c,x) xpos ++ if xpos == width - 1 then "\n" else ""
+  putStrLn $ concat [ pixel (c,x) xpos ++ if xpos == width - 1 then "\n" else ""
                     | (c,x) <- log, let xpos = ((c-1) `mod` width) ]
-  where tick (c,x) ["addx",ns]  = (c+2,x + read ns)
-        tick (c,x) _ {- noop -} = (c+1,x)
-        fillGaps ((c1,x1):(c2,x2):cxs) = if c2 - c1 == 1
-                                         then (c1,x1) :              fillGaps ((c2,x2):cxs)
-                                         else (c1,x1) : (c1+1, x1) : fillGaps ((c2,x2):cxs)
-        fillGaps cx = cx
-        brightDark (cx,rx) c | abs (c - rx) <= 1 = "#"
-                             | otherwise         = " "
+  where tick c x (["addx",ns]:xs)  = [(c+1,x),(c+2,x + read ns)] ++ tick (c+2) (x + read ns) xs
+        tick c x (["noop"]   :xs)  = [(c+1,x)]                   ++ tick (c+1) x             xs
+        tick c x []                = []
+        pixel (cx,rx) c | abs (c - rx) <= 1 = "#"
+                        | otherwise         = " "
