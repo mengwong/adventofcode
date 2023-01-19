2477409d05b065ca303bca02cf999a6ee52aaf6c
Wong Meng Weng <mwwong@smu.edu.sg>
2023-01-20 02:37:01 +0800

    day 11 part 2 naive doesn't work

--- src/Lib11.hs
+++ src/Lib11.hs
@@ -26,15 +26,15 @@ import qualified Data.Tree as Tree
 -- Then we solve part a and b by querying against the Tree.
 
 data Monkey = M { num      :: Int
-                , op       :: Int -> Int
-                , test     :: Int -> Bool -- usually a mod op but who knows what Part 2 will bring?
+                , op       :: Integer -> Integer
+                , test     :: Integer -> Bool -- usually a mod op but who knows what Part 2 will bring?
                 , destT    :: Int
                 , destF    :: Int
                 }
 
 -- which monkey is holding itmes at the start of which round?
-data MonkeyState = MS { holds        :: [Int]
-                      , inspectCount :: Int
+data MonkeyState = MS { holds        :: [Integer]
+                      , inspectCount :: Integer
                       }
   deriving Show
 
@@ -46,7 +46,7 @@ main = do
     Right x -> do
       putStrLn "parse successful!"
       
-      let roundCount = 20
+      let roundCount = 10000
           (monkeys, starting) = unzip x
       finalState <- execStateT (foldM (const $ runRound monkeys) () [1..roundCount]) (DV.fromList starting)
       let monkeyBusiness = product (take 2 (reverse $ sort ( inspectCount <$> DV.toList finalState )))
@@ -56,38 +56,38 @@ main = do
 runMonkey :: Monkey -> StateT (DV.Vector MonkeyState) IO ()
 runMonkey monkey = do
   startState <- get
-  liftIO $ do
-    putStrLn ("Monkey " ++ show monkey.num ++ ":")
+  -- liftIO $ putStrLn ("Monkey " ++ show monkey.num ++ ":")
   let heldItems = holds (startState DV.! monkey.num)
   mapM runItem heldItems
-  monkeyInspects monkey.num (length heldItems)
+  monkeyInspects monkey.num (fromIntegral $ length heldItems)
 
   return ()
     where
-      runItem :: Int -> StateT (DV.Vector MonkeyState) IO ()
+      runItem :: Integer -> StateT (DV.Vector MonkeyState) IO ()
       runItem itemNum = do
         -- we are now inspecting it, so we take it out of the hold
         modify (\st -> st DV.// pure (monkey.num, let oldM = st DV.! monkey.num
                                                   in  oldM { holds = drop 1 oldM.holds }))
         let becomes = monkey.op itemNum
-            div3 = becomes `div` 3
-        liftIO $ putStrLn ("  Monkey inspects an item with a worry level of " ++ show itemNum)
-        liftIO $ putStrLn ("    Worry level becomes " ++ show becomes)
-        liftIO $ putStrLn ("    Monkey gets bored with item. Worry level is divided by 3 to " ++ show div3)
+         -- div3 = becomes `div` 3
+            div3 = becomes
+--        liftIO $ putStrLn ("  Monkey inspects an item with a worry level of " ++ show itemNum)
+--        liftIO $ putStrLn ("    Worry level becomes " ++ show becomes)
+--        liftIO $ putStrLn ("    Monkey gets bored with item. Worry level is not divided by 3 to " ++ show div3)
         if monkey.test div3
           then do
-          liftIO $ putStrLn ("    Item with worry level " ++ show div3 ++ " is thrown to monkey " ++ show monkey.destT)
+--          liftIO $ putStrLn ("    Item with worry level " ++ show div3 ++ " is thrown to monkey " ++ show monkey.destT)
           monkeyTakes monkey.destT div3
           else do
-          liftIO $ putStrLn ("    Item with worry level " ++ show div3 ++ " is thrown to monkey " ++ show monkey.destF)
+--          liftIO $ putStrLn ("    Item with worry level " ++ show div3 ++ " is thrown to monkey " ++ show monkey.destF)
           monkeyTakes monkey.destF div3
         return ()
 
-        where monkeyTakes :: Int -> Int -> StateT (DV.Vector MonkeyState) IO ()
+        where monkeyTakes :: Int -> Integer -> StateT (DV.Vector MonkeyState) IO ()
               monkeyTakes destM itemN =
                 modify (\st -> st DV.// pure (destM, let oldM = st DV.! destM
                                                      in  oldM { holds = oldM.holds ++ [itemN] }))
-      monkeyInspects :: Int -> Int -> StateT (DV.Vector MonkeyState) IO ()
+      monkeyInspects :: Int -> Integer -> StateT (DV.Vector MonkeyState) IO ()
       monkeyInspects mNum increments =
         modify (\st -> st DV.// pure (mNum, let oldM = st DV.! mNum
                                             in  oldM { inspectCount = oldM.inspectCount + increments }))
@@ -96,14 +96,11 @@ runRound :: [Monkey] -> Int -> StateT (DV.Vector MonkeyState) IO ()
 runRound monkeys roundN = do
   mapM_ runMonkey monkeys
   endState <- get
-  liftIO $ putStrLn $ "at the end of round " ++ show roundN ++ ", the monkeys are holding items with these worry levels:"
-  liftIO $ print endState
+  liftIO $ putStrLn $ "at the end of round " ++ show roundN ++ ", the monkeys have counted this many inspections:"
+  liftIO $ print (inspectCount <$> DV.toList endState)
   return ()
     
 type Parser     = Parsec Void String                 -- ^ your basic megaparsec
-
--- | parsers for the input strings.
--- we begin by parsing the input into a list of Traces: cd or ls, followed by a list of files and subdirectories.
 
 type Rounds = [(Monkey, MonkeyState)]
   
@@ -120,7 +117,7 @@ stanzaP = do
   m2 <- hspace1 *> "If false: throw to monkey " *> pInt            <* eol
 
   let mE = \n -> mlEval n mO
-      m  = M mN mE mT m1 m2
+      m  = M (fromIntegral mN) mE mT (fromIntegral m1) (fromIntegral m2)
       ms = MS mI 0
 
   return (m, ms)
@@ -132,7 +129,7 @@ stanzaP = do
     mlEval n (MPlus  mx my) = mlEval n mx + mlEval n my
     
 
-data MathLang = ML Int
+data MathLang = ML Integer
               | MOld
               | MTimes MathLang MathLang
               | MPlus  MathLang MathLang
@@ -146,19 +143,12 @@ mathP = makeExprParser term table <?> "mathLang expression parser"
             , [ binary (try (hspace *> "+")) MPlus ] ]
     binary name f = InfixL (f <$ name)
 
-testP :: Parser (Int -> Bool)
+testP :: Parser (Integer -> Bool)
 testP = do
   mD <- "divisible by " *> pInt
   return (\n -> n `mod` mD == 0)
 
 
-
-
-
--- | utility functions for parsing
-nonWhiteSpace :: Parser String
-nonWhiteSpace = some (alphaNumChar <|> punctuationChar)
-
-pInt :: Parser Int
+pInt :: Parser Integer
 pInt = read <$> some digitChar
 
