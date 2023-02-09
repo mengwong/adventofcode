206d68dcb268e4a4f87fc9c4bab1083dd7949c99
Meng Weng Wong <mengwong@gmail.com>
2023-02-10 01:07:54 +0800

    add comments

--- apps/aoc2022-day13.hs
+++ apps/aoc2022-day13.hs
@@ -10,11 +10,28 @@ import Text.Megaparsec
     ( (<|>), parseMaybe, between, sepBy, some, Parsec )
 import Text.Megaparsec.Char ( digitChar )
 
+-- | We set up a simple minimal Parser.
 type Parser = Parsec () String
 
+-- | for reasons, regular Haskell does not support heterogeneous lists, so we roll our own. 
 data Nested = I Int | N [Nested]
   deriving (Eq)
 
+-- | the input turns out to be pleasantly trivial to parse.
+pNested :: Parser [Nested]
+pNested = between "[" "]" (pInner `sepBy` ",")
+  where
+    pInner = I <$> pInt <|>
+             N <$> pNested
+    pInt :: Parser Int
+    pInt = read <$> some digitChar
+
+-- | we instantiate into Show for the sake of matching the problem statement's solution
+instance Show Nested where
+  show (I n)  = show n
+  show (N ns) = "[" ++ intercalate "," (show <$> ns) ++ "]"
+
+-- | the problem is a lengthy statement of, well, this:
 instance Ord Nested where
   compare (I l)    (I r)    = compare l r
   compare (N [])   (N [])   = EQ
@@ -27,18 +44,7 @@ instance Ord Nested where
   compare (I l)    (N rs)   = compare (N [I l]) (N rs)
   compare (N ls)   (I r )   = compare (N ls) (N [I r])
 
-instance Show Nested where
-  show (I n)  = show n
-  show (N ns) = "[" ++ intercalate "," (show <$> ns) ++ "]"
-
-pNested :: Parser [Nested]
-pNested = between "[" "]" (pInner `sepBy` ",")
-  where
-    pInner = I <$> pInt <|>
-             N <$> pNested
-    pInt :: Parser Int
-    pInt = read <$> some digitChar
-
+-- | with that in hand, we proceed to the solution.
 main :: IO ()
 main = do
   input <- fmap (fromJust . parseMaybe pNested)
@@ -50,15 +56,17 @@ main = do
                             . chunksOf 2
                             $ input
                           ]
-      n2 = [N [N [I 2]]]
+  print $ sum $ fst <$> filter snd results
+
+  -- part 2
+  let n2 = [N [N [I 2]]]
       n6 = [N [N [I 6]]]
       part2 = sort $ n2 : n6 : input
 
-  print $ sum $ fst <$> filter snd results
   -- putStrLn $ unlines $ show <$> part2
 
-  print ( maybe 0 (+1) (n2 `elemIndex` part2)
-        * maybe 0 (+1) (n6 `elemIndex` part2) )
+  print $ maybe 0 (+1) (n2 `elemIndex` part2)
+        * maybe 0 (+1) (n6 `elemIndex` part2)
 
   where answer LT = True
         answer GT = False
